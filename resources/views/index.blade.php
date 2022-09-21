ac88792c33da7871a9594e4c81a70d83261827ae
Kachill4 <friscillachikana@gmail.com>
2022-09-21 18:15:43 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Index</title>
+        <meta http-equiv="Content-Type">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="/css/app.css">
+        <link rel="shortcut icon" href="image/2n2.png">
+    </head>
+
+  <body>
+
+    <div class="container">
+      <div class="nav-scroller py-1 mb-2">
+        <nav class="nav d-flex left-content-between">
+
+          <a class="p-2 text-muted" href="http://127.0.0.1:8000/">Home</a>
+          <a class="p-2 text-muted" href="http://127.0.0.1:8000/portofolio">Portofolio</a>
+        </nav>
+      </div>
+
+      <div class="jumbotron p-3 p-md-5 text-white rounded bg-orange">
+        <div class="col-md-6 px-0">
+        <p>
+            <h1 class="display-4 font-italic">Welcome to My First Website Home.</h1>
+            <p class="lead my-3">
+                This is my first website with bootstrap. <BR>
+                I'm so Interesting to learn about this. <BR>q(^-^)p
+        </p>
+      </p>
+
+        </div>
+      </div>
+
+      <div class="row mb-2">
+        <div class="col-md-6">
+          <div class="card flex-md-row mb-4 box-shadow h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+              <strong class="d-inline-block mb-2 text-primary">Biodata</strong>
+              <h3 class="mb-0"> Chikana Friscilla</h3>
+              <p class="card-text mb-auto">
+                Bandung, 28 November 2001<br>
+                Female<BR>
+                <Address>Maleber Street 21, West Java, Indonesia, 40184</Address>
+                </p>
+            </div>
+            <img class="card-img-right flex-auto d-none d-md-block"
+            data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]"
+            style="width: 200px; height: 250px;" src="image/pic1.jpg">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card flex-md-row mb-4 box-shadow h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+              <strong class="d-inline-block mb-2 text-success">Contact</strong>
+              <h3 class="mb-0"> ichikana28@gmail.com </h3>
+              <p class="card-text mb-auto">
+                Phone: 081386311881</p>
+                <table>
+                    <tr>
+                        <td><img src="image/logoIG.png" style="width: 20px; height: 20px;"></td>
+                        <td>@cfriscilla</td>
+                    </tr>
+                    <tr>
+                        <td><img src="image/logogit.png" style="width: 20px; height: 20px;"></td>
+                        <td>@Kachilla</td>
+                    </tr>
+                    <tr>
+                        <td><img src="image/logoin.png" style="width: 20px; height: 20px;"></td>
+                        <td>Chikana Friscilla</td>
+                    </tr>
+                </table>
+            </div>
+            <img class="card-img-right flex-auto d-none d-md-block"
+            data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]"
+            src="image/pic2.jpg" style="width: 200px; height: 250px;">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <main role="main" class="container">
+      <div class="row">
+        <div class="col-md-8 blog-main">
+          <h3 class="pb-3 mb-4 font-italic border-bottom">
+            About
+          </h3>
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">Introduction</h2>
+
+            <p>Halo, perkenalkan nama saya Chikana Friscilla.
+                Saya berasal dari Kota Bandung dan besar di Bandung juga.
+                Saat ini saya sedang menempuh pendidikan perguruan tinggi
+                di <B>STMIK LIKMI Bandung </B>dan mengambi jurusan Informatika
+                dengan bidang peminatan <B>Design Graphic </B> sejak tahun 2020.
+            </p>
+            <hr>
+          </div><!-- /.blog-post -->
+        </div><!-- /.blog-main -->
+
+        <aside class="col-md-4 blog-sidebar">
+          <div class="p-3 mb-3 bg-light rounded">
+            <h4 class="font-italic">Keahlian</h4>
+            <ol class="list-unstyled mb-0">
+                <li>Mampu bekerja dalam Tim</li>
+                <li>Mampu beradaptasi</li>
+                <li>Mampu bekerja dibawah tekanan</li>
+                <li>Memiliki Orientasi pada layanan</li>
+            </ol>
+          </div>
+
+            <div class="p-3 mb-3 bg-light rounded">
+                <h4 class="font-italic">Softskill</h4>
+                <ol class="list-unstyled mb-0">
+                    <li>Kemauan untuk belajar lebih</li>
+                    <li>Berempati</li>
+                    <li>Percaya diri
+                    <li>Pendengar yang baik</li>
+                    <li>Perhatian ke detail</li>
+                </ol>
+            </div>
+
+          <div class="p-3">
+            <h4 class="font-italic">Hardskill</h4>
+            <ol class="list-group-item mb-0">
+              <li>Copywriting</li>
+              <li>Design Graphic</li>
+              <li>>>Corel Draw</li>
+              <li>>>Adobe Photoshop</li>
+              <li>>>Canva</li>
+              <li>Photo and Video Editing</li>
+              <li>>>Adobe Premiere</li>
+              <li>>>Adobe Animate</li>
+              <li>>>Filmora</li>
+            </ol>
+          </div>
+
+
+        </aside><!-- /.blog-sidebar -->
+
+      </div><!-- /.row -->
+
+    </main><!-- /.container -->
+
+    <footer class="card-footer"><CENTER>
+        <div class="copyright">
+            &copy; Copyright <strong><span>Chikana</span></strong>. All Rights Reserved
+          </div>
+        <a href="views/index.php">Back to top</a>
+      </p></CENTER>
+    </footer>
+
+</body>
+
+</html>
---
+++ resources/views/portofolio.blade.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Index</title>
+        <meta http-equiv="Content-Type">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="/css/app.css">
+        <link rel="shortcut icon" href="image/2n2.png">
+    </head>
+
+  <body>
+
+    <div class="container">
+      <div class="nav-scroller py-1 mb-2">
+        <nav class="nav d-flex left-content-between">
+
+          <a class="p-2 text-muted" href="http://127.0.0.1:8000/">Home</a>
+          <a class="p-2 text-muted" href="http://127.0.0.1:8000/portofolio">Portofolio</a>
+        </nav>
+      </div>
+
+      <div class="jumbotron p-3 p-md-5 text-white rounded bg-cyan">
+        <div class="col-md-6 px-0">
+            <h1 class="display-4 font-italic">Welcome to My First Website of Portofolio.</h1>
+            <p class="lead my-3">
+              This is various of my portofolio.
+              I hope everything of my portofolio can be an inspiration for others.
+              <BR> ヾ(〃^∇^)ﾉ  ヾ('▽'ヾ)
+            </p>
+        </div>
+      </div>
+    </div>
+
+    <main role="main" class="container">
+        <div class="row featurette">
+            <div class="col-md-7">
+              <h2 class="featurette-heading border-bottom">Design Wallpaper for PC  <span class="text-muted">with course schedule.</span></h2>
+              <p class="lead">Ini adalah Wallpaper Laptop yang didesain sendiri.
+                Mengambil beberapa gambar karakter dari internet,
+                lalu di potong bagian pinggirnya untuk di tempelkan pada layer yang sudah di beri warna gradasi yang dibuat sendiri.
+              </p>
+            </div>
+            <div class="col-md-5">
+              <img class="featurette-image img-fluid mx-auto"
+              alt="480x270"
+              src="image/prt1.png">
+            </div>
+        </div> <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-5">
+              <img class="featurette-image img-fluid mx-auto"
+              alt="480x270"
+              src="image/prt0.png">
+            </div>
+            <div class="col-md-7">
+              <h2 class="featurette-heading border-bottom">Design Wallpaper for PC  <span class="text-muted">with course schedule 2.</span></h2>
+              <p class="lead">
+                Ini adalah Desain Walpaper simple, hanya mengambil gambar dari internet, lalu tambahkan jadwal yang saya butuhkan.
+                Selain desain untuk wallpaper PC, dibuat jugalah versi untuk Mobile.
+            </p>
+
+            </div>
+            <div><CENTER>
+                <img class="featurette-image img-fluid mx-auto"
+                    alt="152pt x 270pt"
+                    style="width: 152px; height: 270px;"
+                    src="image/prt04.jpg">
+                <img class="featurette-image img-fluid mx-auto"
+                    alt="152pt x 270pt"
+                    style="width: 152px; height: 270px;"
+                    src="image/prt03.jpg">
+                <img class="featurette-image img-fluid mx-auto"
+                    alt="152pt x 270pt"
+                    style="width: 152px; height: 270px;"
+                    src="image/prt05.png">
+                <img class="featurette-image img-fluid mx-auto"
+                    alt="152pt x 270pt"
+                    style="width: 152px; height: 270px;"
+                    src="image/prt01.png">
+                <img class="featurette-image img-fluid mx-auto"
+                    alt="152pt x 270pt"
+                    style="width: 152px; height: 270px;"
+                    src="image/prt02.png">
+                <img class="featurette-image img-fluid mx-auto"
+                    alt="152pt x 270pt"
+                    style="width: 152px; height: 270px;"
+                    src="image/prt06.jpg">
+            </CENTER></div>
+        </div> <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+              <h2 class="featurette-heading border-bottom">Design Game  <span class="text-muted">(Simple Game)</span></h2>
+              <p class="lead">
+                Ini adalah desain game yang pernah saya buat dengan tema <I>"Shingeki No Kyojin"</I> atau biasa dikenal dengan <I>"Attack on Titan"</I>
+            </p>
+            </div>
+            <div class="col-md-5">
+              <img class="featurette-image img-fluid mx-auto"
+              alt="480x270"
+              src="image/prt3.png">
+            </div>
+            <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto"
+                alt="480x270"
+                src="image/prt4.png">
+              </div>
+            <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto"
+                alt="480x270"
+                src="image/prt5.png">
+            </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto"
+                alt="480x270"
+                src="image/prt6.png">
+              </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto"
+                alt="480x270"
+                src="image/prt7.png">
+              </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto"
+                alt="480x270"
+                src="image/prt8.png">
+              </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto"
+                alt="480x270"
+                src="image/prt9.png">
+              </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto"
+                alt="480x270"
+                src="image/prt10.png">
+              </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto"
+                alt="480x270"
+                src="image/prt11.png">
+              </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto"
+                alt="480x270"
+                src="image/prt12.png">
+              </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto"
+                alt="480x270"
+                src="image/prt13.png">
+              </div>
+        </div> <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+              <h2 class="featurette-heading border-bottom">Another Design  <span class="text-muted">(my project on course).</span></h2>
+              <p class="lead">
+                Selain Desain Wallpaper, ada juga desain lainnya seperti ini. <BR>
+                    <img class="featurette-image img-fluid mx-auto"
+              alt="200x200"
+              style="width: 200px; height: 200px;"
+              src="image/prt2.png">
+
+            </p>
+            </div>
+            <div class="col-md-5">
+              <img class="featurette-image img-fluid mx-auto"
+              alt="480x270"
+              src="image/prt14.jpg">
+            </div>
+        </div> <hr class="featurette-divider">
+
+        </div>
+
+      </div><!-- /.row -->
+      <div class="row mb-2">
+        <div class="col-md-6">
+          <div class="card flex-md-row mb-4 box-shadow h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+              <strong class="d-inline-block mb-2 text-primary">Biodata</strong>
+              <h3 class="mb-0"> Chikana Friscilla</h3>
+              <p class="card-text mb-auto">
+                Bandung, 28 November 2001<br>
+                Female<BR>
+                <Address>Maleber Street 21, West Java, Indonesia, 40184</Address>
+                </p>
+            </div>
+            <img class="card-img-right flex-auto d-none d-md-block"
+            data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]"
+            style="width: 200px; height: 250px;" src="image/pic1.jpg">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card flex-md-row mb-4 box-shadow h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+              <strong class="d-inline-block mb-2 text-success">Contact</strong>
+              <h3 class="mb-0"> ichikana28@gmail.com </h3>
+              <p class="card-text mb-auto">
+                Phone: 081386311881</p>
+                <table>
+                    <tr>
+                        <td><img src="image/logoIG.png" style="width: 20px; height: 20px;"></td>
+                        <td>@cfriscilla</td>
+                    </tr>
+                    <tr>
+                        <td><img src="image/logogit.png" style="width: 20px; height: 20px;"></td>
+                        <td>@Kachilla</td>
+                    </tr>
+                    <tr>
+                        <td><img src="image/logoin.png" style="width: 20px; height: 20px;"></td>
+                        <td>Chikana Friscilla</td>
+                    </tr>
+                </table>
+            </div>
+            <img class="card-img-right flex-auto d-none d-md-block"
+            data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]"
+            src="image/pic2.jpg" style="width: 200px; height: 250px;">
+          </div>
+        </div>
+      </div>
+    </main><!-- /.container -->
+
+    <footer class="card-footer"><CENTER>
+        <div class="copyright">
+            &copy; Copyright <strong><span>Chikana</span></strong>. All Rights Reserved
+          </div>
+        <a href="views/index.php">Back to top</a>
+      </p></CENTER>
+    </footer>
+
+</body>
+
+</html>
